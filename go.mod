7485d8c51c714caf766ba57e70030ab74696a7a6
Shawn Peery <shawn.w8465@gmail.com>
2024-04-06 16:32:05 -0600

    Initial Commit
    - Making repo for this so that I can reference it later
    - Is now determing accurate time for normal speed.
    
    - Will need to determine modifer to update time for other
    types of speed

---
+++ go.mod
@@ -0,0 +1,12 @@
+module notepadtrick/starcrafttwoparser
+
+go 1.22.2
+
+require rsc.io/quote/v4 v4.0.1
+
+require (
+	github.com/icza/mpq v0.0.0-20230330132843-d3cdc0b651b7 // indirect
+	github.com/icza/s2prot v1.5.2-0.20240331065411-f5e7593a4152 // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/icza/s2prot"
+	"github.com/icza/s2prot/rep"
+)
+
+
+func printStructProperties(s interface{}) {
+    v := reflect.ValueOf(s)
+    t := v.Type()
+
+    for i := 0; i < v.NumField(); i++ {
+        field := t.Field(i)
+        value := v.Field(i)
+
+        fmt.Printf("%s: %v\n", field.Name, value.Interface())
+    }
+}
+
+func main() {
+	// r, err := rep.NewFromFile("./Oceanborn LE.SC2Replay")
+
+	r, err := rep.NewFromFile("./TestNormalSpeed.SC2Replay")
+
+
+	if err != nil {
+		fmt.Printf("Failed to open file: %v\n", err)
+		return
+	}
+
+
+	// for _, p := range r.Details.Players() {
+	// 	fmt.Printf("\tName: %-20s, Race: %c, Team: %d, Toon: %s Result: %v\n",
+	// 		p.Name, p.Race().Letter, p.TeamID()+1, p.Toon, p.Result())
+	// }
+
+	// // Initialize variables to track worker count and supply
+	workerCounts := make(map[string]int) // Map of player ID to worker count
+	supplyCounts := make(map[string]int) // Map of player ID to total supply
+
+	// uniqueTrackerEvents := make(map[string]s2prot.Event)
+
+	 uniqueTrackerEvents := []s2prot.Event{} ;
+
+
+	for _, player := range r.Details.Players() {
+		workerCounts[player.Toon.String()] = 0;
+		supplyCounts[player.Toon.String()] = 0;
+	}
+
+
+	for _, evt := range r.TrackerEvts.Evts {
+
+
+		if (strings.ToLower(evt.EvtType.Name) != "unitborn") {
+			// fmt.Printf("%s", lower)
+			continue;
+
+		}
+
+		if (evt.Loop() == 0) {
+			continue;
+		}
+
+		uniqueTrackerEvents = append(uniqueTrackerEvents, evt)
+
+		
+
+
+
+
+
+		// break;
+
+
+		// printStructProperties(evt);
+
+		// uniqueTrackerEvents[evt.EvtType.Name] = struct{}{};
+	}
+
+
+	sort.Slice(uniqueTrackerEvents, func(i, j int) bool {
+		return uniqueTrackerEvents[i].Loop() < uniqueTrackerEvents[j].Loop()
+	})
+
+	for evtIndex := range uniqueTrackerEvents {
+		evt:= uniqueTrackerEvents[evtIndex];
+
+		//  fmt.Printf("\tEvtTypeName: %-20s\n", evt.EvtType.Name);
+
+		// // Calculate in-game time in seconds
+		// inGameTime := float64(evt.Loop()*125) / float64(2*16)
+		//  inGameTime := float64(evt.Loop()* 125 ) / float64(2)
+		realTimeValue:= float64((( evt.Loop() * 125 )) / 2)/1000
+		// inGameTime:= realTimeValue  * (36/26)
+
+		inGameTime:= realTimeValue
+
+
+		printStructProperties(evt);
+
+		// fmt.Println("Loop:", evt.Loop())
+
+		// Print the in-game time
+		fmt.Println("In-game time:", inGameTime, "seconds")
+	}
+
+	
+
+	// // Iterate through game events
+	// for _, evt := range r.GameEvts {
+	// 	// Check if the event is relevant for worker count or supply
+
+	// 	// fmt.Printf("\tName: %-20s, ID %s, TypeName: %s, TypeId: %d\n", evt.Name, evt.ID, evt.EvtType.Name, evt.EvtType.ID)
+	// 	uniqueGameEvents[evt.Name] = struct{}{}
+	// }
+
+	// for key := range uniqueGameEvents {
+
+	// 	 fmt.Printf("\tName: %-20s\n", key);
+	// }
+
+
+
+	// for key, value := range workerCounts {
+
+	// 	fmt.Printf("Key: %s, Value: %d", key, value);
+	// }
+
+
+
+
+	// fmt.Printf("Read file contents! %v\n", r)
+
+	// fmt.Printf("Full Header:\n%v\n", r.Header)
+
+	// fmt.Printf("Version:        %v\n", r.Header.VersionString())
+	// fmt.Printf("Loops:          %d\n", r.Header.Loops())
+	// fmt.Printf("Length:         %v\n", r.Header.Duration())
+	// fmt.Printf("Map:            %s\n", r.Details.Title())
+	// fmt.Printf("Game events:    %d\n", len(r.GameEvts))
+	// fmt.Printf("Message events: %d\n", len(r.MessageEvts))
+	// fmt.Printf("Tracker events: %d\n", len(r.TrackerEvts.Evts))
+
+	// fmt.Println("Players:")
+	// for _, p := range r.Details.Players() {
+	// 	fmt.Printf("\tName: %-20s, Race: %c, Team: %d, Result: %v\n",
+	// 		p.Name, p.Race().Letter, p.TeamID()+1, p.Result())
+	// }
+	defer r.Close()
+
+}
